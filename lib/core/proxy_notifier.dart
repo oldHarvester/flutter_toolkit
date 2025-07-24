16625d3a1c980a9f05576bec2478d9da3f78c78c
Daler <108223768+oldHarvester@users.noreply.github.com>
2025-07-24 09:55:07 +0500

    did ProxyNotifier abstract

--- lib/core/proxy_notifier.dart
+++ lib/core/proxy_notifier.dart
@@ -2,15 +2,15 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter_toolkit/core/custom_value_notifier.dart';
 
 /// `T` - ValueNotifier value, `Z` - listening ValueNotifier, `X` - proxy value from ValueNotifier
-class ProxyNotifier<T, Z extends ValueListenable<T>, X>
+abstract class ProxyNotifier<T, Z extends ValueListenable<T>, X>
     extends CustomValueNotifier<X> {
   ProxyNotifier({
     required this.notifier,
-    required X Function(T value) onProxy,
-  }) : _onProxy = onProxy;
+  });
 
   final Z notifier;
-  final X Function(T value) _onProxy;
+
+  X onProxy(T value);
 
   @override
   void initState() {
@@ -19,12 +19,12 @@ class ProxyNotifier<T, Z extends ValueListenable<T>, X>
   }
 
   void _listener() {
-    setValue(_onProxy(notifier.value));
+    setValue(onProxy(notifier.value));
   }
 
   @override
   X buildState() {
-    return _onProxy(notifier.value);
+    return onProxy(notifier.value);
   }
 
   @override
