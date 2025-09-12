c34959787ee03dea8e5c76ba3ebbb66c7e684a65
Daler <108223768+oldHarvester@users.noreply.github.com>
2025-09-12 11:20:46 +0500

    fix

--- lib/utils/custom_logger.dart
+++ lib/utils/custom_logger.dart
@@ -3,14 +3,31 @@ import 'dart:developer' as dev;
 class CustomLogger {
   CustomLogger({
     required this.owner,
-  });
+    this.showLogs = true,
+  }) {
+    _showLogs = showLogs;
+  }
 
   final String owner;
+  final bool showLogs;
+
+  late bool _showLogs;
+
+  void enableLogs() {
+    _showLogs = true;
+  }
+
+  void disableLogs() {
+    _showLogs = false;
+  }
 
   void log([
     Object? object,
     String? owner,
   ]) {
+    if (!_showLogs) {
+      return;
+    }
     dev.log(
       object.toString(),
       name: owner ?? this.owner,
