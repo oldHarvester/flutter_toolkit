c39013c148085cb37844d11c9cca23eb45d6ac81
Daler <108223768+oldHarvester@users.noreply.github.com>
2025-11-25 14:10:18 +0500

    edited flexible argument completer

--- lib/utils/completers/flexible_argument_completer.dart
+++ lib/utils/completers/flexible_argument_completer.dart
@@ -1,6 +1,11 @@
 import 'package:flutter_toolkit/utils/completers/flexible_completer.dart';
 
 class FlexibleArgumentCompleter<T, Y> extends FlexibleCompleter<T> {
-  FlexibleArgumentCompleter({required this.arg});
+  FlexibleArgumentCompleter({
+    required this.arg,
+    super.onCancel,
+    super.onTimeout,
+    super.timeoutDuration,
+  });
   final Y arg;
 }
