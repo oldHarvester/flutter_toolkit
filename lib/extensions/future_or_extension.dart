d7ee3d77733bf850af0eb5e7b96b05c84e2193c4
Daler <108223768+oldHarvester@users.noreply.github.com>
2025-11-26 17:31:07 +0500

    added safe execute to object extension and also added future or extension

---
+++ lib/extensions/future_or_extension.dart
@@ -0,0 +1,50 @@
+import 'dart:async';
+
+import 'package:flutter_toolkit/flutter_toolkit.dart';
+
+extension FutureOrExtension<T> on FutureOr<T> {
+  WhenValue when<WhenValue>({
+    required WhenValue Function(T value) synchronous,
+    required WhenValue Function(Future<T> value) asynchronous,
+  }) {
+    final futureOr = this;
+    if (futureOr is T) {
+      return synchronous(futureOr);
+    } else {
+      return asynchronous(futureOr);
+    }
+  }
+
+  Future<T>? get asyncValue {
+    return when(
+      synchronous: (value) {
+        return null;
+      },
+      asynchronous: (value) {
+        return value;
+      },
+    );
+  }
+
+  T? get syncValue {
+    return when(
+      synchronous: (value) {
+        return value;
+      },
+      asynchronous: (value) {
+        return null;
+      },
+    );
+  }
+
+  FutureOr<OperationResult<T>> safeExecute() {
+    return when(
+      synchronous: (value) {
+        return value.safeExecute();
+      },
+      asynchronous: (value) {
+        return value.safeExecute();
+      },
+    );
+  }
+}
--- lib/extensions/object_extension.dart
+++ lib/extensions/object_extension.dart
@@ -1,9 +1,22 @@
-extension ObjectExtension on Object {
+import 'package:flutter_toolkit/flutter_toolkit.dart';
+
+extension ObjectExtension<Z extends Object> on Z {
   T? castOrNull<T>() {
     if (this is T) {
       return this as T;
     } else {
       return null;
+    }
+  }
+
+  OperationResult<Z> safeExecute() {
+    try {
+      return OperationResult.success(this);
+    } catch (e, stk) {
+      return OperationResult.failed(
+        error: e,
+        stackTrace: stk,
+      );
     }
   }
 }
