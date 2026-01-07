f16eefef0bd6db2eade118a7288ca292965e3e51
Daler <108223768+oldHarvester@users.noreply.github.com>
2026-01-07 18:49:14 +0500

    changed string extension

--- lib/extensions/string_extension.dart
+++ lib/extensions/string_extension.dart
@@ -1,4 +1,15 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
 extension StringExtension on String {
   bool get hasContent =>
       isNotEmpty && trim().isNotEmpty && replaceAll(' ', '').isNotEmpty;
+
+  Uint8List? get tryBase64Decode {
+    try {
+      return base64Decode(this);
+    } catch (e) {
+      return null;
+    }
+  }
 }
