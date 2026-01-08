dd1e5dc0366b6d42ef59e8a525c1d2de32b5aa98
Daler <108223768+oldHarvester@users.noreply.github.com>
2026-01-08 15:42:42 +0500

    string extension added

--- lib/extensions/string_extension.dart
+++ lib/extensions/string_extension.dart
@@ -1,6 +1,11 @@
 import 'dart:convert';
 import 'dart:typed_data';
 
+enum TruncateDirection {
+  start, // Обрезка с начала
+  end, // Обрезка с конца
+}
+
 extension StringExtension on String {
   bool get hasContent =>
       isNotEmpty && trim().isNotEmpty && replaceAll(' ', '').isNotEmpty;
@@ -22,5 +27,44 @@ extension StringExtension on String {
     } catch (e) {
       return false;
     }
+  }
+
+  String truncateString({
+    int maxLength = 20,
+    String ellipsis = '...',
+    TruncateDirection direction = TruncateDirection.start,
+  }) {
+    final text = this;
+    // Проверка на валидность входных данных
+    if (maxLength <= 0) {
+      return '';
+    }
+
+    // Если строка короче или равна максимальной длине, возвращаем как есть
+    if (text.length <= maxLength) {
+      return text;
+    }
+
+    // Если многоточие длиннее чем максимальная длина, просто обрезаем строку
+    if (ellipsis.length >= maxLength) {
+      return direction == TruncateDirection.end
+          ? text.substring(0, maxLength)
+          : text.substring(text.length - maxLength);
+    }
+
+    // Обрезаем строку с учетом многоточия и направления
+    if (direction == TruncateDirection.end) {
+      // Обрезка с конца (по умолчанию)
+      return text.substring(0, maxLength - ellipsis.length) + ellipsis;
+    } else {
+      // Обрезка с начала
+      return ellipsis +
+          text.substring(text.length - (maxLength - ellipsis.length));
+    }
+  }
+
+  String capitalize() {
+    if (isEmpty) return this;
+    return this[0].toUpperCase() + substring(1).toLowerCase();
   }
 }
