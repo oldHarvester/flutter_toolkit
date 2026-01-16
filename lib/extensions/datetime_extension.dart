2ce3b9f045e2b9126c3d5521f533e15052b9e301
Daler <108223768+oldHarvester@users.noreply.github.com>
2026-01-16 11:54:17 +0500

    added age to DateTimeExtension

--- lib/extensions/datetime_extension.dart
+++ lib/extensions/datetime_extension.dart
@@ -4,4 +4,15 @@ extension DateTimeExtension on DateTime {
   MonthYear get monthYear => MonthYear.fromDate(this);
 
   DateTime get withoutTime => DateTime(year, month, day);
+
+  int get age {
+    final today = DateTime.now();
+    int age = today.year - year;
+
+    if (today.month < month || (today.month == month && today.day < day)) {
+      age--;
+    }
+
+    return age;
+  }
 }
