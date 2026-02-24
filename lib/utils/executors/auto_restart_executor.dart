62b880f342da6648b2778fc37412ed5a4b6f685b
Daler <108223768+oldHarvester@users.noreply.github.com>
2026-02-24 10:34:01 +0500

    added future method to auto restart executor

--- lib/utils/executors/auto_restart_executor.dart
+++ lib/utils/executors/auto_restart_executor.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter_toolkit/core/models/operation_result.dart';
+import 'package:flutter_toolkit/utils/completers/flexible_completer.dart';
 
 typedef AutoRestartExecutorHandler<T> = FutureOr<T> Function();
 
@@ -16,15 +17,24 @@ class AutoRestartExecutor<T> {
   }
 
   final AutoRestartExecutorHandler<T> handler;
+
   final AutoRestartExecutorResultHandler<T> onResult;
+
   final Duration restartDuration;
+
+  final FlexibleCompleter<T> _completer = FlexibleCompleter();
+
+  Future<T?> get future {
+    return _completer.future;
+  }
 
   bool _disposed = false;
 
   bool get disposed => _disposed;
 
-  void dispose() {
+  void dispose([FutureOr<T>? value]) {
     _disposed = true;
+    _completer.cancel(null);
   }
 
   Future<void> _startHandler() async {
@@ -33,6 +43,7 @@ class AutoRestartExecutor<T> {
       final result = await handler();
       if (disposed) return;
       onResult(OperationResult.success(result));
+      _completer.complete(result);
     } catch (e, stk) {
       if (disposed) return;
       onResult(OperationResult.failed(error: e, stackTrace: stk));
