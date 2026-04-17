869e9a89dabdd473095b945fe29b0dfa96a89719
Daler <108223768+oldHarvester@users.noreply.github.com>
2026-04-17 20:03:39 +0500

    added filename method

--- lib/extensions/uri_extension.dart
+++ lib/extensions/uri_extension.dart
@@ -4,4 +4,13 @@ extension UriExtension on Uri {
     final uri = this;
     return '${uri.scheme}://${uri.host}${uri.path}';
   }
+
+  String? get filename {
+    final segments = pathSegments;
+    if (segments.isEmpty) return null;
+    final last = segments.last;
+    final dot = last.lastIndexOf('.');
+    if (dot == -1 || dot == 0 || dot == last.length - 1) return null;
+    return last;
+  }
 }
