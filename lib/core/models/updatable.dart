1e65a4d0ea24989253519e44140981828f3245c1
Daler <108223768+oldHarvester@users.noreply.github.com>
2026-04-23 15:59:42 +0500

    fi

--- lib/core/models/updatable.dart
+++ lib/core/models/updatable.dart
@@ -2,7 +2,7 @@ import 'package:equatable/equatable.dart';
 import 'package:flutter_toolkit/flutter_toolkit.dart';
 
 class Updatable<T> with EquatableMixin {
-  const Updatable({required this.value});
+  const Updatable(this.value);
   final T value;
 
   @override
